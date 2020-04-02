227df0d3288c2baca092f14396bfe178723d75b1
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-04-02 13:49:44 +0300

    Initial commit

---
+++ SampSharpECSTest/MyFirstSystem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using SampSharp.Entities;
+using SampSharp.Entities.SAMP;
+using SampSharp.Entities.SAMP.Commands;
+
+namespace SampSharpECSTest
+{
+    public class MyFirstSystem : ISystem
+    {
+        [Event]
+        public void OnGameModeInit(IServerService serverService)
+        {
+            Console.WriteLine("My first ECS game mode is initializing.");
+
+            serverService.AddPlayerClass(8, new Vector3(0, 0, 7), 0);
+            serverService.SetGameModeText("My First Game Mode");
+        }
+
+        [PlayerCommand("hello")]
+        public void MyFirstCommand(Player player)
+        {
+            player.SendClientMessage($"Hello, {player.Name}!");
+        }
+
+        [Event]
+        public void OnPlayerSpawn(Player player)
+        {
+            player.SendClientMessage("You spawned");
+        }
+
+        [Event]
+        public void OnPlayerRequestSpawn(Player player)
+        {
+            player.SendClientMessage("You request spawn");
+        }
+    }
+}
---
+++ SampSharpECSTest/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SampSharp.Entities;
+using SampSharp.Entities.SAMP;
+
+namespace SampSharpECSTest
+{
+    public class Startup : IStartup
+    {
+        public void Configure(IServiceCollection services)
+        {
+            // TODO: Add services and systems to the services collection
+
+            services.AddSystem<MyFirstSystem>();
+        }
+
+        public void Configure(IEcsBuilder builder)
+        {
+            // TODO: Enable desired ECS system features
+
+            builder.EnableSampEvents()
+                .EnablePlayerCommands()
+                .EnableRconCommands();
+        }
+    }
+}
